4ecb1ef8a3ee6b3fac1bb3618fa10c0028127ae1
Dibyendu-Merlock <mondal007.dibyendu@gmail.com>
2025-07-28 19:33:03 +0530

    Created the project

---
+++ src/test/java/com/dibyendu/ecommercespring/EcommerceSpringApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dibyendu.ecommercespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
